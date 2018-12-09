ae9d3d98434fb51c1920d938c9f450b9203325a1
vmm2297 <vmiranda022097@gmail.com>
2018-12-08 18:53:25 -0800

    car MPG problem with 1 partition

--- svm.py
+++ svm.py
@@ -1,1 +1,10 @@
-print("hello world")
+import numpy as np
+import csv
+
+#Import Datasets
+#1. carPrice
+with open('./datasets/carPrice/car.data.csv', 'rb') as csvfile:
+
+    dataReader = csv.reader(csvfile, delimeter = ',')
+    for row in dataReader:
+        print(row)
